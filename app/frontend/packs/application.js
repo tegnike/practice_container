44074dcd5711fe55796070d44acb658b017f7851
tegnike <user@mbp.local>
2019-08-29 23:05:11 +0200

    set rails-ujs

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -20,3 +20,6 @@ console.log('Hello World from Webpacker')
 import 'stylesheets/application';
 import 'javascripts/application';
 require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
+
+import Rails from 'rails-ujs';
+Rails.start();
