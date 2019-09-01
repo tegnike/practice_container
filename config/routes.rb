e0c9d59451f1845ffaec1e79251d77eeafc5b0d1
tegnike <user@mbp.local>
2019-09-01 06:55:34 +0200

    add sessoin function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
+  devise_for :users
   resources :tasks
   get 'pages/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
